7c02db17460f3149737388be8f1cb5b717c075ba
nb22i345 <nb22i345@MAC-22-093-0.local>
2022-11-24 11:33:11 +0100

    structure and clean and filter data

---
+++ preprocessing/clean_and_filter.r
@@ -0,0 +1,34 @@
+library(haven)
+library(tidyverse)
+
+
+#### SA and TZ ####
+
+satz <- read_dta("data-raw/co2-sa-tz.dta") %>%
+  filter(location == "Your school",
+         grepl("class", tolower(other_location))) %>%
+  mutate(country = ifelse(country == 0, "South Africa", "Tanzania"),
+         date = as.character(date),
+         date = as.POSIXct(date, format = "%Y/%m/%d %H:%M:%S")) %>%
+  select(country, date, carbondioxy) %>%
+  set_names(c("country", "date", "co2"))
+
+saveRDS(satz, "data-clean/co2-sa-tz.rds")
+
+
+#### CH ####
+
+ch <- read_csv("data-raw/co2-ch.csv") %>%
+  filter(!no_school,
+         !no_class,
+         intervention == "No intervention") %>%
+  mutate(country = "Switzerland") %>%
+  mutate(n = ifelse(class == "Study (B)", 14,
+                    ifelse(class == "Study (A)", 24,
+                           ifelse(school == "School 1", 14, 
+                                  ifelse(school == "School 2" & class == "Study", 20, 18))))) %>%
+  rename(co2 = co2ppm) %>%
+  select(country, school, class, date, time, co2, n)
+
+
+saveRDS(ch, "data-clean/co2-ch.rds")
